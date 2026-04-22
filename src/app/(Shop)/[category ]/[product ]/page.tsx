11cb65484081225562721c00a8d5af43dac509b5
Kingsley <olorunfemiolakunle44@gmail.com>
2026-04-22 02:11:41 +0100

    production issue solve

---
+++ src/app/(Shop)/[category ]/[product ]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ProductPage() {
+  return (
+    <div className="container mx-auto py-10 px-4">
+      <h1 className="text-3xl font-bold">Product Detail Page</h1>
+      <p className="mt-4 text-zinc-500">Coming soon...</p>
+    </div>
+  );
+}
